27d0cc451b9ff56795aa5027cace8a6ee80a4fa3
孙振瑜OUCAndroid1-240108 <2441587995@xiaomi.com>
2024-06-02 10:32:17 +0800

    隐式Intent

---
+++ Xiaomi2/app/src/main/java/com/example/xiaomi2/MyActivity.java
@@ -0,0 +1,68 @@
+package com.example.xiaomi2;
+
+import static android.content.ContentValues.TAG;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+        Button button = findViewById(R.id.my_button2);
+        button.setOnClickListener(v -> {
+//            Intent intent=new Intent(MyActivity.this, MyActivity.class);
+//            startActivity(intent);
+            //隐式intent
+            Intent sendIntent = new Intent();
+            sendIntent.setAction(Intent.ACTION_SEND);
+            sendIntent.putExtra(Intent.EXTRA_TEXT,"This is extra data");
+            sendIntent.setType("text/plain");
+            startActivity(sendIntent);
+        });
+        Log.d(TAG, "onCreate: 1");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart: 2");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume: 3");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause: 4");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop: 5");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart: 6");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: 7");
+    }
+}
